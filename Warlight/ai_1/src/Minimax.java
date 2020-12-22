838f97d228ce0589ee3e6d8a97c322bd4b984231
Martin studna <martin.studna2@gmail.com>
2020-12-22 21:14:14 +0100

    feat: progress

--- Warlight/ai_1/src/Minimax.java
+++ Warlight/ai_1/src/Minimax.java
@@ -9,62 +9,81 @@ public class Minimax<S, A> implements Strategy<S, A> {
   private HeuristicGame<S, A> game;
   private int limit;
   private int player;
+  private long debugLevel;
 
   public Minimax(HeuristicGame<S, A> game, int limit) {
     this.game = game;
     this.limit = limit;
+    this.debugLevel = 0;
   }
 
-  private double evaluate(S state, A lastAction, int depth, double alpha, double beta, boolean maximizingPlayer) {
-
-    if (game.isDone(state))
-      return game.outcome(state);
-
-    if (depth == 0 && limit != 0) {
-      return game.evaluate(state);
+  private Result<A> evaluate(S state, int depth, double alpha, double beta, boolean maximizingPlayer) {
+    debugLevel++;
+    System.out.println("Debug level: " + debugLevel);
+    if (game.isDone(state)) {
+      System.out.println(game.outcome(state));
+      return new Result<A>(game.outcome(state), null);
     }
 
-    if (maximizingPlayer) {
-      var maxEval = Double.MAX_VALUE;
-      for (var action : game.actions(state)) {
-        var newState = game.clone(state);
-        game.apply(newState, action);
-        var eval = evaluate(newState, lastAction, depth - 1, alpha, beta, false);
-        if (eval > maxEval && player == 1)
-          lastAction = action;
-        maxEval = Math.max(eval, maxEval);
-        alpha = Math.max(eval, maxEval);
-        lastAction = action;
-        if (beta <= alpha)
-          break;
-      }
-      return maxEval;
-    } else {
-      var minEval = Double.MIN_VALUE;
-      for (var action : game.actions(state)) {
-        var newState = game.clone(state);
-        game.apply(newState, action);
-        var eval = evaluate(newState, lastAction, depth - 1, alpha, beta, false);
-        if (eval < minEval && player == 2)
-          lastAction = action;
-        minEval = Math.min(eval, minEval);
-        alpha = Math.min(eval, minEval);
-        if (beta <= alpha)
-          break;
-      }
+    if (depth == 0 && limit != 0) {
+      System.out.println(game.evaluate(state));
+      return new Result<A>(game.evaluate(state), null);
+    }
 
-      return minEval;
+    A resultAction = null;
+
+    if (maximizingPlayer) {
+      var maxEval = -10000000.0;
+      for (var action : game.actions(state)) {
+        var newState = game.clone(state);
+        game.apply(newState, action);
+        var result = evaluate(newState, depth - 1, alpha, beta, false);
+        debugLevel--;
+        System.out.println("Debug level: " + debugLevel);
+        if (maxEval < result.value) {
+          resultAction = action;
+          maxEval = result.value;
+        }
+
+        if (maxEval >= beta)
+          return new Result<A>(maxEval, resultAction);
+
+        if (maxEval > alpha) {
+          alpha = maxEval;
+        }
+      }
+      System.out.println(maxEval);
+      return new Result<A>(maxEval, resultAction);
+    } else {
+      var minEval = 10000000.0;
+      for (var action : game.actions(state)) {
+        var newState = game.clone(state);
+        game.apply(newState, action);
+        var result = evaluate(newState, depth - 1, alpha, beta, true);
+        debugLevel--;
+        System.out.println("Debug level: " + debugLevel);
+        if (minEval > result.value) {
+          resultAction = action;
+          minEval = result.value;
+        }
+
+        if (minEval <= alpha)
+          return new Result<A>(minEval, resultAction);
+
+        if (minEval < beta) {
+          beta = minEval;
+        }
+      }
+      System.out.println(minEval);
+      return new Result<A>(minEval, resultAction);
     }
   }
 
   // method in Strategy interface
   public A action(S state) {
-    A lastAction = null;
     this.player = game.player(state);
-    evaluate(state, lastAction, limit, Double.MIN_VALUE, Double.MAX_VALUE,
-        game.player(state) == 1 ? true : false);
-
-    return lastAction;
+    var result = evaluate(state, limit, Double.MIN_VALUE, Double.MAX_VALUE, game.player(state) == 1 ? true : false);
+    return result.action;
   }
 
 }
